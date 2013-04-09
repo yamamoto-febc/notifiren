153b9975c3c53e5a40a8dd8108d00cb3e9a74faf
febc.yamamoto <yamamoto.febc@gmail.com>
2013-04-09 22:09:11 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-notiiren
-========
+notifiren
+=========
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notifiren::Application.config.secret_token = 'ef85694a9a9f2507ec38fe27075a8efa27c1ec8a9a11966158393e7a2f3ebd3bd6ab572c1b91dfb6b86cd28fe5818c5966ff0eff151fb9ceb0dc5c0ad3033653'
